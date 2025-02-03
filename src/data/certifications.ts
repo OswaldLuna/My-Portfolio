b87181621c36269fc34f46b475e66ec160a11a63
OswaldoLuna <oswaldo.luna.dev@gmail.com>
2025-02-02 20:27:38 -0600

    first version

---
+++ src/data/certifications.ts
@@ -0,0 +1,38 @@
+export interface Certifications {
+  name: string;
+  provider: string;
+  url: string;
+}
+
+export const certifications: Certifications[] = [
+  {
+    name: "Software Development Engineering",
+    provider: "UNADM",
+    url: "https://www.unadmexico.mx/",
+  },
+  {
+    name: "AWS Fundamentals",
+    provider: "Amazon Web Services",
+    url: "https://github.com/OswaldLuna/certifications/blob/main/AWS-Fundamentals.pdf"
+  },
+  {
+    name: "Advanced React",
+    provider: "Meta",
+    url: "https://github.com/OswaldLuna/certifications/blob/main/Advanced_React.pdf"
+  },
+  {
+    name: "Principles of UX/UI Design",
+    provider: "Meta",
+    url: "https://github.com/OswaldLuna/certifications/blob/main/Principles_of_UX_UI_Design.pdf"
+  },
+  {
+    name: "Version Control",
+    provider: "Meta",
+    url: "https://github.com/OswaldLuna/certifications/blob/main/Version_Control.pdf"
+  },
+  {
+    name: "Introduction to Back-End Development",
+    provider: "Meta",
+    url: "https://github.com/OswaldLuna/certifications/blob/main/Introduction_to_Back-End_Development.pdf"
+  },
+]
